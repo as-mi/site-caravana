3ef101ea4a709401797b23da18e898688e714b21
Alexandru-Vlad <alexandruvlad.adam@gmail.com>
2020-04-07 18:59:41 +0300

    Experimental search engine
    Works just in the console atm
    Test it with searchTerm(str)

---
+++ static/js/search.js
@@ -0,0 +1,185 @@
+let documents = [
+  {
+    title: "Care este diferența dintre specializările domeniului Matematică?",
+    text: `Domeniul Matematică se împarte în 3 specializări:
+
+        ### Matematică pură
+        
+        Absolvenții de **Matematică pură** se îndreaptă către învățământ de toate gradele
+        și către cercetare, acest program oferindu-le o bună aprofundare a noțiunilor teoretice.
+        
+        ### matematici aplicate
+        
+        Absolvenții de **Matematici aplicate** sunt cei care urmează să lucreze
+        ca matematicieni în mediul economic, în special analist de date,
+        cercetare prospectivă sau Data Scientist (mai nou) în bănci,
+        finanțe, asigurări, pensii sau piață. La acest program, se pune
+        accentul pe metode matematice și statistice aplicate în industrie
+        și în economie și software matematic folosit pentru implementarea
+        acestor metode.
+        
+        ### Matematică-Informatică
+        
+        Absolvenții de **Matematică-Informatică** sunt cei care fie au o pasiune
+        atât pentru matematică, cât și pentru informatică și își doresc să le studieze
+        pe amândouă, într-un program comun, la un nivel la care să le permită ulterior
+        să se indrepte in oricare dintre cele 2 direcții.
+        
+        În cadrul programului de Matematică-Informatică se fac 2 ani din planul de
+        învățământ de la Informatică, dar într-un mod mai gradual, care permite
+        și celor care nu știau informatică deloc la intrarea în facultate să ajungă
+        la un nivel suficient de bun și apoi, după primii trei ani de licență,
+        să mai facă un an suplimentar de Informatică și să obțină și licența
+        în Informatică.`,
+  },
+  {
+    title: "Daca mă duc la mate-info pot să iau și licența de la info?",
+    text: `**Da.** Dupa finalizarea cursurilor de licență în domeniul Matematică,
+      vei fi nevoit să dai din nou admiterea la domeniul Informatică și după
+      ce ești admis, vei face doar anul 3 pentru a-ți termina licența în Informatică.`,
+  },
+  {
+    title: "Sportul este obligatoriu?",
+    text: `Doar pentru anul I.`,
+  },
+  {
+    title:
+      "Dacă am intrat la taxă în primul an este vreo posibilitate de a trece la buget ulterior?",
+    text: `**Da.** Înaintea începerii fiecărui an universitar se va realiza o reclasificare
+      unde se va ține cont de rezultatele obținute în anul universitar precedent.`,
+  },
+  {
+    title: "Dacă fac mate-info, pot să fac toată materia de la info?",
+    text: `**Da.** Cât timp ești student la specializarea Matematică–Informatică,
+      vei studia primii 2 ani de la domeniul de licență Informatică, anul 3
+      de la Informatică fiind anul suplimentar pentru a obține a doua licență.`,
+  },
+  {
+    title: "De ce să aleg București când am Cluj și Iași?",
+    text: `Diferențele nu sunt mari între facultățile din aceste orase. În mare
+      parte, trebuie să te gândești în ce oraș ți-ai dori să locuiești și
+      care sunt oportunitățile pe care ți le oferă. În București, de
+      exemplu, fiind un oraș mai mare decat Iași sau Cluj, sunt mai multe
+      companii și mai multe corporații au sedii deschise aici.`,
+  },
+  {
+    title: "Ce oportunități am?",
+    text: `### Internship-uri
+
+    Există multe programe de **internship** în timpul verii. În
+    aceste programe vei fi angajat la o companie și vei învăța ce fac
+    ei pentru a te pregăti mai departe să intri pe piața muncii.
+    Ofertele sunt prezente și specializate pentru toate domeniile
+    noastre de licență și la finalul unui asemenea program, există
+    posibilitatea de a primi o ofertă de a rămâne angajat.
+    
+    ### Workshop-uri
+    Exista diferite **workshop-uri** la care vă puteți înscrie si
+    să obțineți noi skilluri. De exemplu, aveți posibilatea de a vă
+    înscrie în timp la ateliere de Java, Android Development si multe
+    altele.
+    
+    ### Cercetare
+    Dupa absolvire, puteți merge în **cercetare**, să predați în
+    instituțiile de învățământ și să lucrați în domeniul privat, ca
+    angajat în diferite companii pe posturi specific domeniului de
+    licență.`,
+  },
+  {
+    title:
+      "Mă pot muta de la un domeniu la altul? De exemplu, de la CTI la Informatică?",
+    text: `**Nu.** Pentru a te transfera la alt domeniu, trebuie să dai din nou admiterea
+      și să intri la domeniul respectiv. Mai târziu există posibilitatea de a
+      echivala materii comune.`,
+  },
+  {
+    title: "Automatica de la Universitatea Politehnica București vs FMI",
+    text: `Ambele facultăți te pot pregăti pentru o viitoare carieră,
+      depinde foarte mult de ce îți dorești.
+      O să enumerăm câteva diferențe și o recomandare pe care noi o facem.
+      
+      În primul rand de la Facultatea de Automatica si Calculatoare sunteți
+      mai mult îndreptați spre partea de **Computer Engineering**, pe când de la
+      Informatică vă îndreptați spre **Computer Science**. Poate în momentul de față
+      nu vă sunt clare aceste diferențe, dar e ok. Încercăm să le
+      clarificăm în continuare.
+      
+      La Automatică pe lângă cursurile de matematică și informatică,
+      există și cursuri de fizică. Pe lângă partea de software, se face
+      și hardware. Programul de studiu oferit de această facultate se
+      împarte în două specializări: CTI și IS, IS fiind mai axat pe
+      partea de hardware și embedded. Durata programului de studiu este
+      de 4 ani.
+      
+      La multe cursuri pe care le veți sustine la facultatea noastră, va
+      fi nevoie să dezvoltați diferite aplicații software, precum
+      aplicații web, care să respecte diferite cerințe, printre care și
+      integrarea mai multor tehnologii pentru a obține un produs
+      asemănător cu ceea ce găsim în piața de software astăzi.
+      
+      Totuși, dacă sunteți interesați de partea de hardware și embedded,
+din anul 2, aveți posibilitatea de a vă înscrie la cursul de
+robotică unde în primul semestru veți face diferiți roboți
+folosind placute Arduino, vă lăsăm
+[aici](https://www.facebook.com/unibuc.robotics/)
+link către pagina de Facebook a cursului unde sunt prezentați
+roboții făcuți de studenți. La finalul semestrului, în holul
+facultății, se strâng toți studenții cu
+[proiectele pe care le-au realizat](https://www.facebook.com/pg/unibuc.robotics/photos/?tab=album&album_id=2722048637831687)
+și urmează să fie notați de profesorul de curs și profesorii de
+laborator.
+
+În al doilea semestru, puteți continua cursul dacă doriți și se va
+merge pe partea de IoT (Internet of Things). Aici având și mai
+multă libertate în ceea ce privește proiectul pe care doriți să îl
+realizați.
+
+În București, în momentul de față, sunt mai multe companii de
+software și studenții noștrii sunt pregătiți să intre pe piața
+muncii luând în vedere că studeim limbaje de programare și
+tehnologii care sunt folosite în producție. Indiferent de
+facultatea pe care o absolviți dintre cele două, dacă veți lucra
+pe un post de programator, veți vedea că angajații provin din
+ambele instituții.
+
+Recomandarea noastră este să vă uitați pe planurile de învățământ
+ale celor două instituții și să vă gândiți ce vă avantajează cel
+mai mult. Planul nostru de invatamant poate fi găsit
+[aici](http://fmi.unibuc.ro/ro/plan_invatamant_2019_2020/).
+Vă recomandăm să îl studiați și să ne contactați pe pagina
+noastră de Facebook pentru a primi informații suplimentare.
+
+      `,
+  },
+];
+
+let idx = lunr(function () {
+  this.use(lunr.ro);
+
+  this.ref("id");
+  this.field("title");
+  this.field("text");
+
+  let k = 0;
+
+  documents.forEach((doc) => {
+    // remove diacritics for better search indexing
+    doc.title = doc.title.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    doc.text = doc.text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    doc.id = k;
+    k++;
+
+    this.add(doc);
+  }, this);
+});
+
+function searchTerm(str) {
+  // remove diacritics for better search indexing
+  str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+  let results = idx.search(str);
+
+  results = results.map((result) => result.ref);
+
+  console.log(results.map((result) => documents[result]));
+}
